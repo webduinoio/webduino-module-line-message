c7081212b40c490dbfd0de2ea338ca0ded2e6a9d
oxxo <oxxo.studio@gmail.com>
2018-05-16 11:44:31 +0800

    Edit js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -5,7 +5,7 @@ Blockly.JavaScript['line_ifttt'] = function(block) {
   var value_line_ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_line_ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
   
-  var code = '$.get("https://maker.ifttt.com/trigger/'+ value_line_ifttt_event + '/with/key/' + value_line_ifttt_key + '?value=' +value_line_ifttt_value1'+")';
+  var code = '$.get("https://maker.ifttt.com/trigger/'+ value_line_ifttt_event + '/with/key/' + value_line_ifttt_key + '?value=' +value_line_ifttt_value1 + '")';
 
   return code;
 };

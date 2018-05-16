cd49ae147d8d715a59d590aab1bc32dca1cfaec1
oxxo <oxxo.studio@gmail.com>
2018-05-16 16:08:16 +0800

    Fixed bug

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -5,6 +5,18 @@ Blockly.JavaScript['line_ifttt'] = function(block) {
   var v2 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
   var v3 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
 
+  var content = '';
+
+  if(v1.length == 0){
+  	v1 = "''";
+  }
+  if(v2.length == 0){
+  	v2 = "''";
+  }
+  if(v3.length == 0){
+  	v3 = "''";
+  }
+
   var code = 'line_ifttt(' + event + ',' + key + ',' + v1 + ',' + v2 + ',' + v3 + ');';
 
   return code;

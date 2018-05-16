d9740689ead0341d96834ff95bc16c2155a35348
oxxo <oxxo.studio@gmail.com>
2018-05-16 11:29:16 +0800

    Add js code

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -4,7 +4,8 @@ Blockly.JavaScript['line_ifttt'] = function(block) {
   var value_line_ifttt_value1 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_line_ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_line_ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'line_ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  
+  var code = '$.get("https://maker.ifttt.com/trigger/" + value_line_ifttt_event + "/with/key/" + value_line_ifttt_key + "?value=" +value_line_ifttt_value1)';
+
   return code;
 };

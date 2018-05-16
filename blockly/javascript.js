08cfac353b82fd9e35c82242af42dee77be31942
oxxo <oxxo.studio@gmail.com>
2018-05-16 15:20:54 +0800

    edit js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -8,27 +8,27 @@ Blockly.JavaScript['line_ifttt'] = function(block) {
   var arr = [];
   var code;
 
-  var js = '$.get("https://maker.ifttt.com/trigger/';
+  var js = '$.get("https://maker.ifttt.com/trigger/"+';
 
   if (v1.length > 0) {
-    arr.push('value1=' + v1);
+    arr.push('"value1="+' + v1);
   }
   if (v2.length > 0) {
-    arr.push('value2=' + v2);
+    arr.push('"value2="+' + v2);
   }
   if (v3.length > 0) {
-    arr.push('value3=' + v3);
+    arr.push('"value3="+' + v3);
   }
 
   switch (arr.length) {
     case 1:
-      code = js + event + key + '?' + arr[0] + '")';
+      code = js + event + '+"/width/key/"+' + key + '+"?"+' + arr[0] + ')';
       break;
     case 2:
-      code = js + event + key + '?' + arr[0] + '&' + arr[1] + '")';
+      code = js + event + '+"/width/key/"+' + key + '+"?"+' + arr[0] + '+"&"+' + arr[1] + ')';
       break;
     case 3:
-      code = js + event + key + '?' + arr[0] + '&' + arr[1] + '&' + arr[2] + '")';
+      code = js + event + '+"/width/key/"+' + key + '+"?"+' + arr[0] + '+"&"+' + arr[1]+ '+"&"+' + arr[2] + ')';
       break;
 
   }

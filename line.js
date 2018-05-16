57cefb098df8a8c871bb7d20a292af00d4a9f432
oxxo <oxxo.studio@gmail.com>
2018-05-16 16:02:29 +0800

    edit js

--- line.js
+++ line.js
@@ -6,13 +6,13 @@
 
     var obj = {};
 
-    if (v1 != "''") {
+    if (v1 != "''" || v1.length > 0) {
       obj.value1 = v1;
     }
-    if (v2 != "''") {
+    if (v2 != "''" || v1.length > 0) {
       obj.value2 = v2;
     }
-    if (v3 != "''") {
+    if (v3 != "''" || v1.length > 0) {
       obj.value3 = v3;
     }
 
